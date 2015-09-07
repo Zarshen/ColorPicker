f7c099bcb073af202784474f87523dfedc2608c0
Braden Parks <bpp226@vt.edu>
2015-09-07 19:34:31 -0400

    Removed unnecessary comments.

--- src/cs3744/hw1/HW1Controller.java
+++ src/cs3744/hw1/HW1Controller.java
@@ -1,7 +1,5 @@
 package cs3744.hw1;
 
-/*Takes in instances of both the model and view and sets itself as a listener in both others*/
-
 public class HW1Controller {
 
     public HW1Controller(HW1View view, HW1Model model) {
--- src/cs3744/hw1/HW1Model.java
+++ src/cs3744/hw1/HW1Model.java
@@ -1,21 +1,3 @@
-/*Data objects
-        color object or 3 doubles
-    // Properties
-        Simple Double Properties**
-        Simple Object Properties
-    // Initial Values of Properties
-        0
-    // Allowed Values of Properties
-        0-1
-    //How are properties put together in a single data container
-        Public class HW1Model{
-            as i have it
-        }
-    //How can controller update properties and listen to properies changes?
-        get/set value methods
-    */
-
-
 package cs3744.hw1;
 
 import javafx.beans.property.DoubleProperty;
